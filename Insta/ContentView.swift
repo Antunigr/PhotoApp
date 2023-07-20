7c13a8031ad1a2262ea8b2481a962373ea272753
Antuni <antuniribeiro@gmail.com>
2023-07-20 10:16:10 -0300

    Initial Commit

---
+++ Insta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Insta
+//
+//  Created by antuni gr on 20/07/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
